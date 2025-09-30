28d7e1d600e51d12e2dbf088d3f21f120c912d3a
Nischal Acharya <Nischal060@gmail.com>
2025-09-30 17:25:36 +0545

    fixes for host

--- config/db.php
+++ config/db.php
@@ -4,6 +4,11 @@ $user = "root";
 $pass = "";
 $db   = "khaneapani";
 
+//$host = "sql313.infinityfree.com";
+//$user = "if0_39987491";
+//$pass = "hiukHPgcZJ1HCuT";
+//$db   = "if0_39987491_khanepani";
+
 $conn = mysqli_connect($host, $user, $pass, $db);
 
 if (!$conn) {
