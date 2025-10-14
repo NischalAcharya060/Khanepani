999c6083924779bdf29dfa2a39ee8aa9ad16d444
Nischal Acharya <88274356+NischalAcharya060@users.noreply.github.com>
2025-10-14 07:37:50 +0545

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,13 +1,22 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "khaneapani";
+// LocalHost
+// $host = "localhost";
+// $user = "root";
+// $pass = "";
+// $db   = "khaneapani";
 
+// Infinity Host
 //$host = "sql313.infinityfree.com";
 //$user = "if0_39987491";
 //$pass = "hiukHPgcZJ1HCuT";
 //$db   = "if0_39987491_khanepani";
+
+// Wasmer Host
+$host: db.fr-pari1.bengt.wasmernet.com
+$port: 10272
+$db: khanepani
+$user: da83b3f17a968000a4066df68a56
+$pass: 068eda83-b3f1-7c82-8000-834f83407111
 
 $conn = mysqli_connect($host, $user, $pass, $db);
 
